063ab388319ecb3b3ff6f0c5dc192210595d0c3c
Aiden Cullo <aidencullo@gmail.com>
2024-09-24 11:53:05 -0400

    Navigation

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
-
-// Import Route and our components
 import { Route, Routes } from "react-router-dom";
 import Currencies from "./pages/currencies";
 import Main from "./pages/main";
 import Price from "./pages/price";
+import Nav from "./components/nav";
 
 export default function App () {
-  // We will use the Route component, path attribute, and element attribute to specify each route.
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<Main/>}/>
         <Route path="/currencies" element={<Currencies/>}/>
